3681532211e872853e412f50fe38789933d9e6d8
MaciejDziuban <dziuban.maciej@gmail.com>
2023-11-09 15:56:48 +0100

    Improve error checking

--- main.cpp
+++ main.cpp
@@ -9,11 +9,9 @@
 
 #include <iostream>
 
-#define FATAL_ERROR(message)                  \
-    {                                         \
-        printf("FATAL ERROR: %s\n", message); \
-        throw 1;                              \
-    }
+#define REPORT_INFO() std::cout
+#define REPORT_ERROR() std::cerr << "ERROR: "
+#define REPORT_WARNING() std::cerr << "WARNING: "
 
 struct KnownDriver {
     const char *vendorName;
@@ -30,27 +28,33 @@ const KnownDriver knownDrivers[] = {
 #endif
 };
 
-void initGl() {
+bool initGl() {
     // Create window class
     WNDCLASSA wc = {};
     wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
     wc.lpfnWndProc = DefWindowProcA;
     wc.hInstance = GetModuleHandle(0);
-    wc.lpszClassName = "Dummy_WGL_djuasioadaddwa";
+    wc.lpszClassName = "WinGfxDriverDetectClass";
     if (!RegisterClassA(&wc)) {
-        FATAL_ERROR("Failed to register dummy OpenGL window.");
+        REPORT_ERROR() << "Failed to register window class.\n";
+        return false;
     }
 
     // Create window
-    HWND hwnd = CreateWindowExA(0, wc.lpszClassName, "Dummy OpenGL Window", 0,
+    HWND hwnd = CreateWindowExA(0, wc.lpszClassName, "WinGfxDriverDetectWindow", 0,
                                 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
                                 CW_USEDEFAULT, 0, 0, wc.hInstance, 0);
     if (!hwnd) {
-        FATAL_ERROR("Failed to create dummy OpenGL window.");
+        REPORT_ERROR() << "Failed to create dummy window.\n";
+        return false;
     }
     HDC deviceContext = GetDC(hwnd);
+    if (!deviceContext) {
+        REPORT_ERROR() << "Failed to get device context of dummy window.\n";
+        return false;
+    }
 
-    // Create pixel format
+    // Set pixel format for window
     PIXELFORMATDESCRIPTOR pfd = {
         sizeof(PIXELFORMATDESCRIPTOR),
         1,
@@ -78,53 +82,70 @@ void initGl() {
         0,
         0,
         0};
-    int pixelFormat = ChoosePixelFormat(deviceContext, &pfd);
-    if (!SetPixelFormat(deviceContext, pixelFormat, &pfd)) {
-        FATAL_ERROR("Pixel format failed");
+    const int pixelFormat = ChoosePixelFormat(deviceContext, &pfd);
+    if (pixelFormat == 0) {
+        REPORT_ERROR() << "Failed to choose pixel format for dummy window.\n";
+        return false;
+    }
+    if (SetPixelFormat(deviceContext, pixelFormat, &pfd) == 0) {
+        REPORT_ERROR() << "Failed to set pixel format for dummy window.\n";
+        return false;
     }
 
-    // Create GL context
+    // Create GL context. We will not release it, to avoid any module unloading.
     HGLRC glContext = wglCreateContext(deviceContext);
+    if (glContext == nullptr) {
+        REPORT_ERROR() << "Failed to create GL context.\n";
+        return false;
+    }
+
+    return true;
 }
 
 std::wstring getOpenglDriverStore() {
-    initGl();
+    if (!initGl()) {
+        return {};
+    }
 
     HANDLE process = GetCurrentProcess();
 
     DWORD neededSpace = 0;
     if (EnumProcessModules(process, nullptr, 0, &neededSpace) == 0) {
-        return L"";
+        REPORT_ERROR() << "EnumProcessModules failed. GetLastError()=0x" << std::hex << GetLastError() << "\n";
+        return {};
     }
 
     const DWORD modulesCount = neededSpace / sizeof(HMODULE);
     auto modules = std::make_unique<HMODULE[]>(modulesCount);
-    if (EnumProcessModules(process, modules.get(), modulesCount * sizeof(HMODULE),
-                           &neededSpace) == 0) {
-        return L"";
+    if (EnumProcessModules(process, modules.get(), modulesCount * sizeof(HMODULE), &neededSpace) == 0) {
+        REPORT_ERROR() << "EnumProcessModules failed. GetLastError()=0x" << std::hex << GetLastError() << "\n";
+        return {};
     }
 
     for (DWORD i = 0u; i < modulesCount; i++) {
         TCHAR moduleName[MAX_PATH];
         if (GetModuleFileNameEx(process, modules[i], moduleName, MAX_PATH) == 0) {
-            return L"";
+            REPORT_ERROR() << "GetModuleFileNameEx failed. GetLastError()=0x" << std::hex << GetLastError() << "\n";
+            return {};
         }
 
         for (const KnownDriver &knownDriver : knownDrivers) {
             char *ptr = strstr(moduleName, knownDriver.dllName);
-            if (ptr != nullptr) {
-                size_t moduleDirLength =
-                    (ptr - moduleName) - 1; // -1 for trailing backslash
-                auto moduleDir = std::make_unique<char[]>(moduleDirLength + 1);
-                strncpy(moduleDir.get(), moduleName, moduleDirLength);
-                std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-                std::wstring wide = converter.from_bytes(moduleDir.get());
-                return wide;
+            if (ptr == nullptr) {
+                continue;
             }
+
+            size_t moduleDirLength = (ptr - moduleName) - 1; // -1 for trailing backslash
+            auto moduleDir = std::make_unique<char[]>(moduleDirLength + 1);
+            strncpy(moduleDir.get(), moduleName, moduleDirLength);
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+            std::wstring wide = converter.from_bytes(moduleDir.get());
+            return wide;
         }
     }
 
-    return L"";
+    REPORT_ERROR() << "No OpenGL driver found.\n";
+    return {};
 }
 
 bool compareStringCaseInsensitive(const std::wstring &a,
@@ -140,11 +161,10 @@ bool compareStringCaseInsensitive(const std::wstring &a,
 
 int main() {
     std::wstring actualStore = getOpenglDriverStore();
-    if (!actualStore.empty()) {
-        std::wcout << actualStore << '\n';
-        return 0;
-    } else {
-        std::wcerr << "No OpenGL driver found.";
-        return 3;
+    if (actualStore.empty()) {
+        return EXIT_FAILURE;
     }
+
+    std::wcout << actualStore << '\n';
+    return EXIT_SUCCESS;
 }
